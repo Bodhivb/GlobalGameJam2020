3b12f1c92b624e340b4c017ecaeb1c7b4b69cb67
Rick Rijkers <rick.rijkers1@gmail.com>
2020-01-31 22:57:00 +0100

    updated player with model

--- Assets/Player/Scripts/PlayerAbilitys/WalkAbility.cs
+++ Assets/Player/Scripts/PlayerAbilitys/WalkAbility.cs
@@ -2,50 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WalkAbility : Ability , IPlayerAbilitys {
-	private PlayerController _playerController;
-	public float speed = 1;
+public class WalkAbility : Ability, IPlayerAbilitys
+{
+    private PlayerController _playerController;
+    public float speed = 1;
 
-	public override void OnStart(){
-		_playerController = GetComponent<PlayerController> ();
-	}
+    public override void OnStart()
+    {
+        _playerController = GetComponent<PlayerController>();
+    }
 
-	public override void EveryFrame(){
-		float x = Input.GetAxis("Player" + _playerController.player.ToString() + "Horizontal");
-		float z = Input.GetAxis("Player" + _playerController.player.ToString() + "Vertical");
-		if (AbilityPermitted)
-		{
-			Vector3 forge = new Vector3 (0, 0, 0);
-			if (_playerController.canWalkRight && x > 0)
-			{
-				forge.x += x;
-			}
-			if (_playerController.canWalkLeft && x < 0)
-			{
-				forge.x += x;
-			}
-			if (_playerController.canWalkForward && z > 0)
-			{
-				forge.z += z;
-			}
-			if (_playerController.canWalkBack && z < 0)
-			{
-				forge.z += z;
-			}
-			transform.Translate (forge * Time.deltaTime * speed);
-		}
-	}
+    public override void EveryFrame()
+    {
+        float x = Input.GetAxis("Player" + _playerController.player.ToString() + "Horizontal");
+        float z = Input.GetAxis("Player" + _playerController.player.ToString() + "Vertical");
+        if (AbilityPermitted)
+        {
+            Vector3 movement = new Vector3(x, 0.0f, z);
+            if (x != 0 || z != 0)
+                transform.rotation = Quaternion.LookRotation(movement);
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        }
+    }
 
-	public override void BeforeAbility(){
+    public override void BeforeAbility()
+    {
 
-	}
+    }
 
-	public override void WhileAbility(){
+    public override void WhileAbility()
+    {
 
-	}
+    }
 
-	public override void AfterAbility(){
+    public override void AfterAbility()
+    {
 
-	}
+    }
 }
 
