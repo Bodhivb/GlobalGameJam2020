fbb31fbad2b75787b84ac37643da5666e62adb9f
Rick Rijkers <rick.rijkers1@gmail.com>
2020-01-31 22:10:41 +0100

    Added stunner

---
+++ Assets/Player/Scripts/PlayerAbilitys/StunAbility.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunAbility : Ability, IPlayerAbilitys
+{
+	private PlayerController _playerController;
+	[SerializeField]
+	float timeStunned = 1.0f;
+	float timer;
+	bool stunned = false;
+	public Ability[] abilities;
+	public override void OnStart()
+	{
+		_playerController = GetComponent<PlayerController>();
+	}
+
+	public void Stun()
+	{
+		timer = timeStunned;
+		stunned = true;
+		foreach(Ability a in abilities)
+		{
+			a.AbilityPermitted = false;
+		}
+	}
+	public override void EveryFrame()
+	{
+		if (AbilityPermitted)
+		{
+			if (stunned)
+			{
+				timer -= Time.deltaTime;
+				if(timer <= 0)
+				{
+					stunned = false;
+					foreach (Ability a in abilities)
+					{
+						a.AbilityPermitted = true;
+					}
+				}
+			}
+		}
+	}
+
+	public override void BeforeAbility()
+	{
+
+	}
+
+	public override void WhileAbility()
+	{
+
+	}
+
+	public override void AfterAbility()
+	{
+
+	}
+}
+
---
+++ Assets/Scripts/Stunner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stunner : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           StunAbility sa = other.GetComponent<StunAbility>();
+            if (sa != null)
+                sa.Stun();
+        }
+    }
+}
