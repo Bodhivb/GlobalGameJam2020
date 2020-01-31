4e0bf925c20d1a1a4a6eab4541ca55bf57df8697
Rick Rijkers <rick.rijkers1@gmail.com>
2020-01-31 23:37:42 +0100

    Added meter

---
+++ Assets/Scripts/Meter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meter : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointer;
+    float map(float s, float a1, float a2, float b1, float b2)
+    {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+    }
+    public void SetMeter(float value)
+    {
+        float v = map(value, 0, 100, 0, 360);
+        pointer.transform.rotation = Quaternion.Slerp(pointer.transform.rotation, Quaternion.Euler(pointer.transform.rotation.x, pointer.transform.rotation.y, 180-v), 1);
+    }
+}
