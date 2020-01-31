6f771bb12e6176003fd2b7a6405ff0a0ded7b77f
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2020-01-31 23:45:52 +0100

    Added basic game manager

--- Assets/Scripts/Defective/DefectiveObject.cs
+++ Assets/Scripts/Defective/DefectiveObject.cs
@@ -44,7 +44,7 @@ public class DefectiveObject : MonoBehaviour, IInteraction
     // Start is called before the first frame update
     void Start()
     {
-        Defect();
+        //Defect();
     }
 
     public void Defect()
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.LogWarning("There is already instance of GameManager in the game");
+            Destroy(this);
+        }
+    }
+
+    #endregion
+
+    [SerializeField] private Meter meter;
+
+    private DefectiveObject[] defectives;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defectives = FindObjectsOfType<DefectiveObject>();
+        SpawmRandomDefect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meter.SetMeter(Time.time);
+    }
+
+    void SpawmRandomDefect()
+    {
+        if (defectives.Length > 1)
+            defectives[Random.Range(0, defectives.Length - 1)].Defect();
+    }
+}
