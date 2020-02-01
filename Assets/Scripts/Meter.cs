5b875989eba726f44e50673cfaa658480a593323
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2020-02-01 20:26:15 +0100

    Added boiler effect by meter

--- Assets/Scripts/Meter.cs
+++ Assets/Scripts/Meter.cs
@@ -7,6 +7,11 @@ public class Meter : MonoBehaviour
     [SerializeField]
     Transform pointer;
     public float value;
+
+    [SerializeField]
+    ParticleSystem whiteSmoke;
+
+
     float map(float s, float a1, float a2, float b1, float b2)
     {
         return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
@@ -15,5 +20,22 @@ public class Meter : MonoBehaviour
     {
         float v = map(value, 0, 100, 0, 360);
         pointer.transform.localRotation = Quaternion.Slerp(pointer.transform.localRotation, Quaternion.Euler(pointer.transform.localRotation.x, pointer.transform.localRotation.y, 180 + v), 1);
+
+        if (value > 95)
+        {
+            whiteSmoke.startSize = 9;
+        }
+        else if (value > 75)
+        {
+            whiteSmoke.startSize = 7;
+        }
+        else if (value > 50)
+        {
+            whiteSmoke.startSize  = 5;
+        }
+        else
+        {
+            whiteSmoke.startSize = 3;
+        }
     }
 }
