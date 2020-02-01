aff836ea03767e8afeeee89a0336c230220f8ff0
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2020-02-02 00:57:12 +0100

    Improvement spawn object

--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -4,14 +4,27 @@ public class SpawnObject : MonoBehaviour
 {
     [SerializeField] private GameObject obj;
     [SerializeField] private float sizeSpawn = 3f;
+    [SerializeField] private Transform target;
 
+    [SerializeField] private GameObject[] defectsObject;
+
+
+    public void Start()
+    {
+        defectsObject = new GameObject[11];
+    }
 
     public void Spawn()
     {
-        Instantiate(obj, new Vector3(
-            transform.position.x + Random.Range(-sizeSpawn, sizeSpawn),
-            transform.position.y,
-            transform.position.z + Random.Range(-sizeSpawn, sizeSpawn)),
-            Quaternion.identity);
+        for (int i = 0; i < defectsObject.Length; i++)
+        {
+            if (defectsObject[i] == null)
+            {
+                Vector3 pos = Vector3.Lerp(transform.position, target.position, 0.09f * i);
+
+                defectsObject[i] = Instantiate(obj, new Vector3(pos.x + Random.Range(-sizeSpawn, sizeSpawn), pos.y, pos.z + Random.Range(-sizeSpawn, sizeSpawn)), Quaternion.identity) as GameObject;
+                return;
+            }
+        }
     }
 }
