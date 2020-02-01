da1a8b671226661466a2b1107ef8f32eb2ad148a
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2020-02-01 04:32:29 +0100

    Added basic game menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public int currentPlayers = 0;
+    public readonly int minPlayers = 2;
+
+    public UnityEngine.UI.Button playButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.interactable = false;
+    }
+
+    public void AddPlayer()
+    {
+        currentPlayers++;
+        if (currentPlayers >= minPlayers)
+        {
+            //Game is ready to begin
+            playButton.interactable = true;
+            playButton.GetComponentInChildren<UnityEngine.UI.Text>().text = "Press enter to play";
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (currentPlayers >= minPlayers)
+            {
+                StartGame();
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/PlayerUIInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerUIInput : MonoBehaviour
+{
+    public MainMenu menu;
+    public int playerInput;
+
+    public GameObject pressJoin;
+    public GameObject joined;
+
+    public GameObject readyText;
+    public bool isReady;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressJoin.SetActive(true);
+        joined.SetActive(false);
+        readyText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Player" + playerInput.ToString() + "Intersect"))
+        {
+            if (pressJoin.activeSelf)
+            {
+                pressJoin.SetActive(false);
+                joined.SetActive(true);
+                readyText.SetActive(true);
+                menu.AddPlayer();
+            }
+        }
+    }
+
+
+}
