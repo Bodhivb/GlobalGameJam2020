2b9ee3f2a7c851bdd84da0e5421dbc5c94eb518c
Rick Rijkers <rick.rijkers1@gmail.com>
2020-02-01 10:56:10 +0100

    added pump and bucket

---
+++ Assets/Player/Scripts/Interactible/Crane.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crane : MonoBehaviour
+{
+    public ParticleSystem particle;
+    public Bucket bucket;
+    private void Start()
+    {
+
+        particle.enableEmission = false;
+    }
+    public void GiveWater()
+    {
+        particle.enableEmission = true;
+        StopAllCoroutines();
+        StartCoroutine(TurnOffWater());
+        if (bucket != null)
+        {
+            bucket.filled += 5;
+        }
+    }
+    IEnumerator TurnOffWater()
+    {
+        yield return new WaitForSeconds(1.0f);
+        particle.enableEmission = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        bucket = other.GetComponent<Bucket>();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        bucket = null;
+    }
+}
