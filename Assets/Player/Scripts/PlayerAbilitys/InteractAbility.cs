90533dca6665df04333eb96b05d24d50089100bc
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2020-02-02 12:06:53 +0100

    Added action icon Interactive

--- Assets/Player/Scripts/PlayerAbilitys/InteractAbility.cs
+++ Assets/Player/Scripts/PlayerAbilitys/InteractAbility.cs
@@ -7,6 +7,14 @@ public class InteractAbility : Ability, IPlayerAbilitys
 {
     private PlayerController _playerController;
     private PickUpAbility _pickUpAbility;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite xboxA;
+    [SerializeField] Sprite xboxAR;
+    [SerializeField] Sprite playA;
+    [SerializeField] Sprite playAR;
+    [SerializeField] Sprite pcE;
+    [SerializeField] Sprite pcER;
+
     StunAbility stunAbility;
     IInteractible inter;
     IInteractible pickUp;
@@ -14,13 +22,34 @@ public class InteractAbility : Ability, IPlayerAbilitys
     bool playstation;
     bool redPicture;
     int frames = 0;
+    [SerializeField] Item water;
+
     public override void OnStart()
     {
         _playerController = GetComponent<PlayerController>();
         _pickUpAbility = GetComponent<PickUpAbility>();
+        spriteRenderer.enabled = false;
         if (Input.GetJoystickNames().Length >= _playerController.player)
         {
-            playstation = "Wireless Controller" == Input.GetJoystickNames()[_playerController.player - 1];
+            Debug.Log(Input.GetJoystickNames()[_playerController.player - 1]);
+            playstation = "Sony Computer Entertainment Wireless Controller" == Input.GetJoystickNames()[_playerController.player - 1];
+            if (!playstation)
+                playstation = "Wireless Controller" == Input.GetJoystickNames()[_playerController.player - 1];
+
+            if (playstation)
+            {
+                Debug.Log("playstation control");
+                spriteRenderer.sprite = playA;
+            }
+            else
+            {
+                Debug.Log("xbox control");
+                spriteRenderer.sprite = xboxA;
+            }
+        } else
+        {
+            Debug.Log("pc control");
+            spriteRenderer.sprite = pcE;
         }
     }
 
@@ -28,14 +57,54 @@ public class InteractAbility : Ability, IPlayerAbilitys
     {
         if (AbilityPermitted)
         {
-           
+            if (spriteRenderer.enabled)
+            {
+                if (frames > 30)
+                {
+                    frames = 0;
+                    if (redPicture)
+                    {
+                        redPicture = false;
+                        if (spriteRenderer.sprite.name.ToString() == xboxAR.name.ToString())
+                        {
+                            spriteRenderer.sprite = xboxA;
+                        }
+                        else if (spriteRenderer.sprite.name.ToString() == playAR.name.ToString())
+                        {
+                            spriteRenderer.sprite = playA;
+                        }
+                        else if (spriteRenderer.sprite.name.ToString() == pcER.name.ToString())
+                        {
+                            spriteRenderer.sprite = pcE;
+                        }
+                    }
+                    else
+                    {
+                        redPicture = true;
+
+                        if (spriteRenderer.sprite.name.ToString() == xboxA.name.ToString())
+                        {
+                            spriteRenderer.sprite = xboxAR;
+                        }
+                        else if (spriteRenderer.sprite.name.ToString() == playA.name.ToString())
+                        {
+                            spriteRenderer.sprite = playAR;
+                        }
+                        else if (spriteRenderer.sprite.name.ToString() == pcE.name.ToString())
+                        {
+                            spriteRenderer.sprite = pcER;
+                        }
+                    }
+                }
+                frames++;
+            }
+
             if (playstation)
             {
                 if (Input.GetButtonDown("Player" + _playerController.player.ToString() + "IntersectP"))
                 {
                     BeforeAbility();
                 }
-
             }
             else
             {
@@ -95,19 +164,33 @@ public class InteractAbility : Ability, IPlayerAbilitys
         if (c.CompareTag("Defect"))
         {
             defect = c.GetComponent<DefectiveObject>();
+            if (defect.objectHealth == DefectiveObject.ObjectHealth.defect)
+            {
+               if (_pickUpAbility.pickUpItem != null && _pickUpAbility.pickUpItem.item == defect.repairItem) {
+                    spriteRenderer.enabled = true;
+                }
+            }
         }
+
         if (c.CompareTag("Interactable"))
         {
+            if (_pickUpAbility.pickUpItem != null && _pickUpAbility.pickUpItem.item == water)
+                spriteRenderer.enabled = true;
+
             inter = c.GetComponent<IInteractible>();
         }
         if (c.CompareTag("PickUp"))
         {
+            if (pickUp == null && !_pickUpAbility.hasItem)
+                spriteRenderer.enabled = true;
+
             pickUp = c.GetComponent<IInteractible>();
         }
         if (c.CompareTag("Player"))
         {
             if (c.transform != this.transform)
             {
+                spriteRenderer.enabled = true;
                 stunAbility = c.GetComponent<StunAbility>();
             }
         }
@@ -117,6 +200,7 @@ public class InteractAbility : Ability, IPlayerAbilitys
     {
         if (c.CompareTag("Interactable") && inter == null)
         {
+            spriteRenderer.enabled = true;
             inter = c.GetComponent<IInteractible>();
         }
     }
@@ -126,6 +210,7 @@ public class InteractAbility : Ability, IPlayerAbilitys
         {
             if (defect != null)
             {
+                spriteRenderer.enabled = false;
                 defect.CanceldRepair();
                 defect = null;
             }
@@ -134,15 +219,18 @@ public class InteractAbility : Ability, IPlayerAbilitys
         {
             if (inter == c.GetComponent<IInteractible>())
             {
+                spriteRenderer.enabled = false;
                 inter = null;
             }
         }
         if (c.CompareTag("PickUp"))
         {
+            spriteRenderer.enabled = false;
             pickUp = null;
         }
         if (c.CompareTag("Player"))
         {
+            spriteRenderer.enabled = false;
             stunAbility = null;
         }
     }
